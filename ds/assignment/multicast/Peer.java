c6178577c5100995995d36a82e0ea3b747b52c4e
André Cerqueira <acerqueira021@gmail.com>
2022-12-27 12:33:30 +0000

    multicast: redo got the idea wrong

--- ds/assignment/multicast/Peer.java
+++ ds/assignment/multicast/Peer.java
@@ -9,18 +9,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * Peer
- */
 public class Peer {
 
     static final AtomicLong timestamp = new AtomicLong();
     static final ArrayList<String> neighbours = new ArrayList<>();
+    static final ArrayList<String> servers_that_offer_service = new ArrayList<>();
 
     public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException {
 
@@ -29,20 +25,25 @@ public class Peer {
             System.exit(1);
         }
 
-        for (int i = 2; i < args.length; i++) {
+        // TODO: Dont like this way of doing input at all ...
+        int i = 0;
+        for (i = 2; i < args.length - 2; i++) {
             Peer.neighbours.add(args[i]);
+        }
+
+        for (; i < args.length; i++) {
+            Peer.servers_that_offer_service.add(args[i]);
         }
 
         new Thread(new Server(args[0], args[1])).start();
         new Thread(new Client()).start();
+
     }
 }
 
 class Server implements Runnable {
     ServerSocket server;
     String ip_port, ip_addr;
-    static final PriorityQueue<Message> queue = new PriorityQueue<>();
-    static final HashMap<InetAddress, ArrayList<Message>> source = new HashMap<>();
 
     public Server(String ip_addr, String ip_port) throws NumberFormatException, UnknownHostException, IOException {
         this.ip_port = ip_port;
@@ -54,65 +55,68 @@ class Server implements Runnable {
     public void run() {
         while (true) {
             try {
-
                 Socket client = server.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                InetAddress sender = client.getInetAddress();
-
-                Message message_received = new Message().parse(in.readLine());
-
-                if (!source.containsKey(sender)) {
-                    source.put(sender, new ArrayList<Message>());
-                }
-                ArrayList<Message> list_of_messages = source.get(sender);
-                list_of_messages.add(message_received);
-
-                long tc;
-                for (;;) {
-                    long old_value = Peer.timestamp.get();
-                    tc = Math.max(old_value, message_received.getTimestamp()) + 1;
-                    if (Peer.timestamp.compareAndSet(old_value, tc))
-                        break;
-                }
-
-                if (!queue.contains(message_received)) {
-                    Server.send_message(new Message(tc, message_received.getMessage()));
-                }
-
-                Server.queue.add(message_received);
-
-                while (!Server.queue.isEmpty()) {
-                    Message tmp = Server.queue.poll();
-
-                    if (!queue.contains(tmp)) {
-                        System.out.println(tmp);
-                    }
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-    static void send_message(Message message_to_send) {
-        for (String client : Peer.neighbours) {
-            try {
-                String client_data[] = client.split(":");
-
-                Socket socket = new Socket(InetAddress.getByName(client_data[0]), Integer.parseInt(client_data[1]));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-                out.println(message_to_send.toString());
-                out.flush();
-
-                socket.close();
+                new Thread(new Connection(client)).start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    static void send_message(Message message_to_send) throws NumberFormatException, UnknownHostException, IOException {
+        message_to_send.setType_of_message("time_update");
+        for (String tmp : Peer.neighbours) {
+            String ip_to_send[] = tmp.split(":");
+
+            Socket socket = new Socket(InetAddress.getByName(ip_to_send[0]), Integer.parseInt(ip_to_send[1]));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message_to_send);
+            out.flush();
+            out.close();
+        }
+        message_to_send.setType_of_message("message");
+        for (String tmp : Peer.servers_that_offer_service) {
+            String ip_to_send[] = tmp.split(":");
+
+            Socket socket = new Socket(InetAddress.getByName(ip_to_send[0]), Integer.parseInt(ip_to_send[1]));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message_to_send);
+            out.flush();
+            out.close();
+        }
+    }
+}
+
+class Connection implements Runnable {
+    Socket socket;
+
+    public Connection(Socket client) {
+        this.socket = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner parser = new Scanner(in.readLine());
+            Message message_received = new Message().parse(parser.nextLine());
+            switch (message_received.getType_of_message()) {
+                case "message:":
+                    System.out.println(message_received.toString()); // TODO: what does the prof means by replica do I
+                                                                     // need to send
+                    // the message to p6 aswell (assuming I am p5)
+                case "time_update:":
+                    long old_value = Peer.timestamp.get();
+                    Peer.timestamp.compareAndSet(old_value, message_received.getTimestamp() + 1);
+                    break;
+                default:
+                    System.err.println("Invalid Call");
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
@@ -126,11 +130,13 @@ class Client implements Runnable {
     @Override
     public void run() {
         while (true) {
-            String input = scanner.nextLine();
-            Message to_send = new Message(Peer.timestamp.incrementAndGet(), input);
-
-            Server.send_message(to_send);
-
+            try {
+                String input = scanner.nextLine();
+                Message to_send = new Message(Peer.timestamp.getAndIncrement(), input);
+                Server.send_message(to_send);
+            } catch (NumberFormatException | IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -138,6 +144,7 @@ class Client implements Runnable {
 class Message implements Comparable<Message> {
     private Long timestamp;
     private String message;
+    private String type_of_message;
 
     public Message(Long timestamp, String message) {
         this.timestamp = timestamp;
@@ -146,6 +153,7 @@ class Message implements Comparable<Message> {
 
     public Message parse(String readLine) {
         Scanner sc = new Scanner(readLine);
+        this.type_of_message = sc.next();
         this.timestamp = Long.parseLong(sc.next());
         this.message = sc.nextLine();
         sc.close();
@@ -168,7 +176,8 @@ class Message implements Comparable<Message> {
 
     @Override
     public String toString() {
-        return timestamp + " " + message;
+        return type_of_message + ": " + timestamp + " " + message; // TODO: probably remove the type_of_message from
+                                                                   // toString()
     }
 
     public Long getTimestamp() {
@@ -186,4 +195,13 @@ class Message implements Comparable<Message> {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public String getType_of_message() {
+        return type_of_message;
+    }
+
+    public void setType_of_message(String type_of_message) {
+        this.type_of_message = type_of_message;
+    }
+
 }
