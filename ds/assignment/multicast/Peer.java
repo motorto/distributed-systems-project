1058a5c25db85229fdb1dfe7d5fdd841e04069b1
André Cerqueira <acerqueira021@gmail.com>
2023-01-01 11:47:38 +0000

    multicast: missing the ack + checking the queue

--- ds/assignment/multicast/Peer.java
+++ ds/assignment/multicast/Peer.java
@@ -1,6 +1,7 @@
 package ds.assignment.multicast;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,25 +10,23 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Peer {
-
     static final AtomicLong timestamp = new AtomicLong();
-    static final ArrayList<String> neighbours = new ArrayList<>();
-    static final ArrayList<String> servers_that_offer_service = new ArrayList<>();
+    static HashMap<String, Socket> neighbours = new HashMap<>();
+    // static HashMap<String, Socket> servers_that_offer_service = new HashMap<>();
     static boolean i_am_a_server = false;
 
-    public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException {
-
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         if (args.length < 3) {
             System.err.println("Missing Arguments: Peer my_ip my_port file_with_ip_of_all_networks");
             System.exit(1);
         }
 
-        boolean i_am_a_server = false;
         try (BufferedReader br = new BufferedReader(new FileReader(args[2]))) {
             String line;
             String my_ip = args[0] + ":" + args[1];
@@ -38,12 +37,12 @@ public class Peer {
                         Peer.i_am_a_server = true;
                         continue;
                     }
-                    Peer.servers_that_offer_service.add(line.replace("s:", ""));
+                    Peer.neighbours.put(line_tmp, null);
                 } else {
                     if (line.equals(my_ip)) {
                         continue;
                     }
-                    Peer.neighbours.add(line);
+                    Peer.neighbours.put(line, null);
                 }
             }
         }
@@ -59,11 +58,13 @@ public class Peer {
 class Server implements Runnable {
     ServerSocket server;
     String ip_port, ip_addr;
+    static PriorityQueue<Message> queue;
 
     public Server(String ip_addr, String ip_port) throws NumberFormatException, UnknownHostException, IOException {
         this.ip_port = ip_port;
         this.ip_addr = ip_addr;
         this.server = new ServerSocket(Integer.parseInt(ip_port), 1, InetAddress.getByName(ip_addr));
+        Server.queue = new PriorityQueue<>();
     }
 
     @Override
@@ -78,22 +79,34 @@ class Server implements Runnable {
         }
     }
 
-    static void send_message(Message message_to_send) throws NumberFormatException, UnknownHostException, IOException {
-        message_to_send.setType_of_message("time_update");
-        for (String tmp : Peer.neighbours) {
-            String ip_to_send[] = tmp.split(":");
-
-            Socket socket = new Socket(InetAddress.getByName(ip_to_send[0]), Integer.parseInt(ip_to_send[1]));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(message_to_send);
-            out.flush();
-            out.close();
+    public static void update_clocks(Message message_received) {
+        long old_value = Peer.timestamp.get();
+        while (true) {
+            if (Peer.timestamp.compareAndSet(old_value, Math.max(old_value, message_received.getTimestamp() + 1))) {
+                break;
+            }
         }
-        message_to_send.setType_of_message("message");
-        for (String tmp : Peer.servers_that_offer_service) {
-            String ip_to_send[] = tmp.split(":");
+    }
 
-            Socket socket = new Socket(InetAddress.getByName(ip_to_send[0]), Integer.parseInt(ip_to_send[1]));
+    private static void build_network() throws NumberFormatException, UnknownHostException, IOException {
+        for (String ip_tmp : Peer.neighbours.keySet()) {
+            if (Peer.neighbours.get(ip_tmp) == null) {
+                String ip_to_create_connection[] = ip_tmp.split(":");
+                Socket socket = new Socket(InetAddress.getByName(ip_to_create_connection[0]),
+                        Integer.parseInt(ip_to_create_connection[1]));
+                Peer.neighbours.replace(ip_tmp, null, socket);
+            }
+        }
+    }
+
+    public static void send_message(Message message_to_send)
+            throws NumberFormatException, UnknownHostException, IOException {
+
+        if (Peer.neighbours.containsValue(null)) {
+            Server.build_network();
+        }
+
+        for (Socket socket : Peer.neighbours.values()) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(message_to_send);
             out.flush();
@@ -115,30 +128,45 @@ class Connection implements Runnable {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner parser = new Scanner(in.readLine());
             Message message_received = new Message().parse(parser.nextLine());
+
+            System.out.println("DEBUG");
+            System.out.println(message_received);
+            System.out.println("----");
+
             switch (message_received.getType_of_message()) {
                 case "message":
-                    System.out.println(message_received.toString()); // TODO: what does the prof means by replica do I
-                                                                     // need to send
-                    // the message to p6 aswell (assuming I am p5)
-                case "time_update":
-                    long old_value = Peer.timestamp.get();
-                    Peer.timestamp.compareAndSet(old_value, Math.max(old_value, message_received.getTimestamp() + 1));
-
-                    if (!Peer.i_am_a_server) {
-                        System.out.println("Update Clock");
+                    Server.update_clocks(message_received);
+                    Server.queue.add(message_received);
+                    // Server.send_ack(message_received);
+                    break;
+                case "acknowledge":
+                    Server.update_clocks(message_received);
+                    if (!Server.queue.isEmpty()) {
+                        Server.queue.add(message_received);
                     }
                     break;
                 default:
                     System.err.println("Invalid Call");
                     break;
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
 }
 
+// TODO: Change this to use poisson;
+
+// start_poisson_generator()
+// lamport_clock = 0
+// while(true) {
+//       dt = next_poisson_interval()
+//       sleep(dt)
+//       send_message(lamport_clock++)
+// }
 class Client implements Runnable {
     private Scanner scanner;
 
@@ -152,6 +180,7 @@ class Client implements Runnable {
             try {
                 String input = scanner.nextLine();
                 Message to_send = new Message(Peer.timestamp.getAndIncrement(), input);
+                to_send.setType_of_message("message");
                 Server.send_message(to_send);
             } catch (NumberFormatException | IOException e) {
                 e.printStackTrace();
